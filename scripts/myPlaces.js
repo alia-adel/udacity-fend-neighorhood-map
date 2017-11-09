8a91bfb6e2a1286eb213a83e3b97658f927094cd
alia-adel <alia.adel.83@gmail.com>
2017-11-09 21:13:03 +0200

    refactor: Move map center control's styles to main.css

--- scripts/myPlaces.js
+++ scripts/myPlaces.js
@@ -344,24 +344,13 @@ function setMapToPosition(lat, lng, zoom = 20) {
 function CenterControl(controlDiv, map) {
     // Set CSS for the control border.
     let controlUI = document.createElement('div');
-    controlUI.style.backgroundColor = '#fff';
-    controlUI.style.border = '2px solid #fff';
-    controlUI.style.borderRadius = '3px';
-    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-    controlUI.style.cursor = 'pointer';
-    controlUI.style.marginBottom = '22px';
-    controlUI.style.textAlign = 'center';
+    $(controlUI).addClass('center-ctrl-out');
     controlUI.title = 'Click to recenter the map';
     controlDiv.appendChild(controlUI);
 
     // Set CSS for the control interior.
     let controlText = document.createElement('div');
-    controlText.style.color = 'rgb(25,25,25)';
-    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-    controlText.style.fontSize = '16px';
-    controlText.style.lineHeight = '38px';
-    controlText.style.paddingLeft = '5px';
-    controlText.style.paddingRight = '5px';
+    $(controlText).addClass('center-ctrl-inner');
     controlText.innerHTML = 'Center Map';
     controlUI.appendChild(controlText);
 
